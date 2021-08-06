7ae7d748f001ba90274175aab5dc13017e8b36ba
Tusharshrivastav <ts3657@gmail.com>
2021-08-06 11:15:54 +0530

    calendar feature

--- components/Header.js
+++ components/Header.js
@@ -3,9 +3,32 @@ import {
   SearchIcon,
   MenuIcon,
   UserCircleIcon,
+  UsersIcon,
   GlobeAltIcon,
 } from "@heroicons/react/solid";
+import { useState } from "react";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { DateRangePicker } from "react-date-range";
 const Header = () => {
+  const [searchInput, setsearchInput] = useState("");
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setendDate] = useState(new Date());
+  const [numberofGuests, setnumberofGuests] = useState(1);
+
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate);
+    setendDate(ranges.selection.endDate);
+  };
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+
+  const resatInput = () => {
+    setsearchInput("");
+  };
   return (
     <header
       className="sticky top-0 z-50 grid grid-cols-3
@@ -28,6 +51,8 @@ const Header = () => {
       md:shadow-sm"
       >
         <input
+          value={searchInput}
+          onChange={(e) => setsearchInput(e.target.value)}
           className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600"
           type="text"
           placeholder="Start your search"
@@ -49,6 +74,39 @@ const Header = () => {
           <UserCircleIcon className="h-6" />
         </div>
       </div>
+      {searchInput && (
+        <div
+          className="flex flex-col col-span-3 mx-auto
+       mt-5 "
+        >
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={["#fd5861"]}
+            onChange={handleSelect}
+          />
+          <div className="flex items-center border-b mb-4">
+            <h2 className="text-2xl flex-grow font-semibold ">
+              Number of Guests
+            </h2>
+            <UsersIcon className="h-5" />
+            <input
+              type="number"
+              className="
+            w-12 pl-2 text-lg outline-none text-red-400"
+              value={numberofGuests}
+              min={1}
+              onChange={(e) => setnumberofGuests(e.target.value)}
+            />
+          </div>
+          <div className="flex">
+            <button className="flex-grow text-gray-500" onClick={resatInput}>
+              Cancel
+            </button>
+            <button className="flex-grow text-red-400">Search</button>
+          </div>
+        </div>
+      )}
     </header>
   );
 };
---
+++ pages/search.js
@@ -0,0 +1,9 @@
+const search = () => {
+  return (
+    <div>
+      <h1>i am the search </h1>
+    </div>
+  );
+};
+
+export default search;
